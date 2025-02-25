f2f7ddf69d70b2f61622ffab2ce7f902228761df
Dmitry Nesterenok <nesterenokda@gmail.com>
2025-02-25 16:45:57 +0400

    Initial version darwing mobile

--- android/app/src/main/kotlin/com/example/dart_wing_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dart_wing_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/opensoft/inkrouter/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.opensoft.dartwing
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+    // ...
+}
